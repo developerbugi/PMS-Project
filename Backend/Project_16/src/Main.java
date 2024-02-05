0dcc744ff23ca145a9727e4b024ee54e2c62807b
거부길 <sopens@naver.com>
2024-02-05 14:21:00 +0900

    사원 삭제

--- Backend/Project_16/src/Main.java
+++ Backend/Project_16/src/Main.java
@@ -1,19 +1,13 @@
-import db_connection.dao.insertDAO;
-import db_connection.dao.selectDAO;
+import db_connection.dao.deleteDAO;
+
+
 
 
 
 public class Main {
     public static void main(String[] args) {
-        insertDAO td2 = new insertDAO();
+        deleteDAO td3 = new deleteDAO();
 
-        String result = td2.insertMember("9876543210", "김철수", "Kim Cheol-su",
-                "서울시 강서구", "계약직", "2023-03-15",
-                "", "010-9876-5432", "",
-                "8012345678901", "kimcs@example.com", "면제",
-                "고등학교 졸업", "영어", "10일",
-                "8일");
-
-        System.out.println(result);
+        System.out.println(td3.deleteEmployee("1234567890"));
     }
 }
