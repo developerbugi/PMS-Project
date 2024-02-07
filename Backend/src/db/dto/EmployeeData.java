d0fa173f4f53c69e5eaaed21b6e031eea4bd8b66
거부길 <sopens@naver.com>
2024-02-07 15:56:14 +0900

    dto , main 복구

---
+++ Backend/src/db/dto/EmployeeData.java
@@ -0,0 +1,131 @@
+package db.dto;
+
+public class EmployeeData {
+    private String com_id;
+    private String name_kor;
+    private String name_eng;
+    private String address;
+    private String emp_type;
+    private String emp_hiredate;
+    private String mod_num;
+    private String rrn;
+    private String emp_email;
+    private String military;
+    private String final_edu;
+    private String major;
+
+    private String dep_id;
+    private String rank_id;
+
+    public String getCom_id() {
+        return com_id;
+    }
+
+    public void setCom_id(String com_id) {
+        this.com_id = com_id;
+    }
+
+    public String getName_kor() {
+        return name_kor;
+    }
+
+    public void setName_kor(String name_kor) {
+        this.name_kor = name_kor;
+    }
+
+    public String getName_eng() {
+        return name_eng;
+    }
+
+    public void setName_eng(String name_eng) {
+        this.name_eng = name_eng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmp_type() {
+        return emp_type;
+    }
+
+    public void setEmp_type(String emp_type) {
+        this.emp_type = emp_type;
+    }
+
+    public String getEmp_hiredate() {
+        return emp_hiredate;
+    }
+
+    public void setEmp_hiredate(String emp_hiredate) {
+        this.emp_hiredate = emp_hiredate;
+    }
+
+    public String getMod_num() {
+        return mod_num;
+    }
+
+    public void setMod_num(String mod_num) {
+        this.mod_num = mod_num;
+    }
+
+    public String getRrn() {
+        return rrn;
+    }
+
+    public void setRrn(String rrn) {
+        this.rrn = rrn;
+    }
+
+    public String getEmp_email() {
+        return emp_email;
+    }
+
+    public void setEmp_email(String emp_email) {
+        this.emp_email = emp_email;
+    }
+
+    public String getMilitary() {
+        return military;
+    }
+
+    public void setMilitary(String military) {
+        this.military = military;
+    }
+
+    public String getFinal_edu() {
+        return final_edu;
+    }
+
+    public void setFinal_edu(String final_edu) {
+        this.final_edu = final_edu;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getDep_id() {
+        return dep_id;
+    }
+
+    public void setDep_id(String dep_id) {
+        this.dep_id = dep_id;
+    }
+
+    public String getRank_id() {
+        return rank_id;
+    }
+
+    public void setRank_id(String rank_id) {
+        this.rank_id = rank_id;
+    }
+}
