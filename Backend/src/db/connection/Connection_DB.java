45ee27a3764277c626ba2ecdc6085b26f38575d4
거부길 <sopens@naver.com>
2024-02-07 15:52:26 +0900

    C,R,U,D 복구

---
+++ Backend/src/db/connection/Connection_DB.java
@@ -0,0 +1,33 @@
+package db.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connection_DB {
+    private static Connection conn = null;
+
+    public static Connection GetDB()
+    {
+
+        String url = "jdbc:mysql://localhost:3306/pmt_table";
+        String user = "root";
+        String password = "as10048782";
+
+        if (conn != null)
+        {
+            return conn;
+        }
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn =  DriverManager.getConnection(url, user, password);
+            return conn;
+        } catch (Exception e)
+        {
+            System.out.println("커넥션 획득 실패");
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
