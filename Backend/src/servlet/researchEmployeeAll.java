5f59ec3f974910b8870f4de552d53b4cd09ae806
거부길 <sopens@naver.com>
2024-02-08 15:54:35 +0900

    URL 수정

--- Backend/src/servlet/researchEmployeeAll.java
+++ Backend/src/servlet/researchEmployeeAll.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("api/employees/all")
+@WebServlet("/api/employees/all")
 public class researchEmployeeAll extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         selectDAO dao = new selectDAO();
