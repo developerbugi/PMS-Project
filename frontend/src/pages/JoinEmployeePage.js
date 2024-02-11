154e64aece47ea190951cf2103ef3eafc9e5574a
hwiyoon20010309 <boksil0101@gmail.com>
2024-02-11 23:29:34 +0900

    Update JoinEmployeePage.js

--- frontend/src/pages/JoinEmployeePage.js
+++ frontend/src/pages/JoinEmployeePage.js
@@ -1,10 +1,110 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import employeeImage from '../img/Employee.png';
-
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; 
+// import { useRecoilState } from 'recoil';
+// import { searchState } from './states';
+
+const SWrap = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-image: linear-gradient(
+    90deg,
+    #82b2f8 28.12%,
+    #477ebf 65.62%,
+    #477ebf 100%
+  );
+`;
+
+const EmployeeSystemContainer = styled.div`
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background: #f9f9f9;
+`;
+
+const Header = styled.header`
+  h1 {
+    font-size: 24px;
+    color: #333;
+    margin-bottom: 20px;
+  }
+`;
+
+const Row = styled.div`
+
+  display: flex;
+  margin-bottom: 20px;
+
+  &.first-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+`;
+
+const SearchSection = styled.div`
+  display: flex;
+  align-items: center;
+
+  label,
+  input,
+  button {
+    margin-right: 10px;
+  }
+`;
+
+const ControlButtons = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const InputField = styled.input`
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+  
+
+  &:focus {
+    border-color: #699CFF;  
+    outline: none;  
+  }
+`;
+
+const TextAreaField = styled.textarea`
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  padding: 10px 15px;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  background-color: #699CFF;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #477EBF;
+  }
+`;
+
 
 const JoinEmployeePage = () => {
+
+  // const navigate = useNavigate();
+  // const [search, setSearch] = useRecoilState(searchState);
 
   const [employee, setEmployee] = useState({
     name_kor: "", //이름
@@ -23,18 +123,23 @@ const JoinEmployeePage = () => {
     address: "", //주소
   });
 
-
-
   const handleInputChange = (e) => {
     setEmployee({ ...employee, [e.target.name]: e.target.value });
     
   };
 
     const handleSubmit = async (event) => {
-      // 이벤트의 기본 동작을 중단
-    // 폼 제출에 의한 페이지 새로고침을 막는 역할
+      // 이벤트의 기본 동작을 중단 (폼 제출에 의한 페이지 새로고침을 막는 역할)
       event.preventDefault();
   
+      // 모든 필드가 입력되었는지 확인
+      for (let key in employee) {
+        if (employee[key] === "") {
+          alert('아직 입력하지 않은 항목이 있습니다');
+          return;
+        }
+      }
+
       // API 호출을 위한 데이터를 설정
       const apiData = {
         ...employee,
@@ -47,6 +152,8 @@ const JoinEmployeePage = () => {
         console.log(response.data);
       // API 호출이 성공하면 알림을 표시
       alert('등록되었습니다');
+      // setSearch('');  // recoil 상태를 초기화
+      // history.push('/search');  // '/search' 페이지로 이동
       } catch (error) {
         console.error(error);
       }
@@ -63,7 +170,10 @@ const JoinEmployeePage = () => {
           <h1>사원 등록</h1>
           </Header>
             <ControlButtons >
-              <Button onClick={handleSubmit}>
+              <Button onClick={handleSubmit}
+              style = {{
+                marginRight : '20px'
+              }}>
                 등록 </Button>                   
             </ControlButtons>
             
@@ -76,7 +186,7 @@ const JoinEmployeePage = () => {
             style = {{
               width : '90px',
               height : '100px', 
-              marginRight: '10px'
+              marginLeft : '20px'
             }}
             />
             <div>
@@ -94,7 +204,7 @@ const JoinEmployeePage = () => {
                   value={employee.name_kor}
                   onChange={handleInputChange}
                   placeholder="ex) 최휘윤"
-                  style = {{maxWidth :'100px'}}
+                  
                 />
 
                 <label htmlFor="name_eng"
@@ -304,7 +414,7 @@ const JoinEmployeePage = () => {
               <label htmlFor="email"
               style = {{
                 width :'55px',
-                textAlign: 'center'
+                textAlign: 'right'
                 }}>
                   이메일 :</label>
               <InputField
@@ -313,6 +423,7 @@ const JoinEmployeePage = () => {
                 name="email"
                 value={employee.email}
                 onChange={handleInputChange}
+                style = {{marginLeft : '20px'}}
                 placeholder="ex) seohan@gmail.com"
               />
  
@@ -329,7 +440,23 @@ const JoinEmployeePage = () => {
                 name="address"
                 value={employee.address}
                 onChange={handleInputChange}
+                style = {{marginLeft : '20px'}}
                 placeholder="ex) 경기도 용인시 수지구 서한로 123번길 10 서한아파트 101동 101호"
+              />
+            </Row>
+            <Row>
+              <label htmlFor="address"
+              style = {{
+                width :'55px',
+                textAlign: 'center'
+                }}>기타 특이사항 :</label>
+              <InputField
+                type="text"
+                id="address"
+                name="address"
+                value={employee.address}
+                onChange={handleInputChange}
+                style = {{marginLeft : '20px'}}
               />
             </Row>
             
@@ -343,97 +470,6 @@ const JoinEmployeePage = () => {
   );
 };
 
-const SWrap = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background-image: linear-gradient(
-    90deg,
-    #82b2f8 28.12%,
-    #477ebf 65.62%,
-    #477ebf 100%
-  );
-`;
 
-const EmployeeSystemContainer = styled.div`
-  max-width: 800px;
-  margin: auto;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  background: #f9f9f9;
-`;
-
-const Header = styled.header`
-  h1 {
-    font-size: 24px;
-    color: #333;
-    margin-bottom: 20px;
-  }
-`;
-
-const Row = styled.div`
-
-  display: flex;
-  margin-bottom: 20px;
-
-  &.first-row {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-  }
-`;
-
-const SearchSection = styled.div`
-  display: flex;
-  align-items: center;
-
-  label,
-  input,
-  button {
-    margin-right: 10px;
-  }
-`;
-
-const ControlButtons = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const InputField = styled.input`
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 100%;
-
-  &:focus {
-    border-color: #699CFF;  
-    outline: none;  
-  }
-`;
-
-const TextAreaField = styled.textarea`
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 100%;
-`;
-
-const Button = styled.button`
-  padding: 10px 15px;
-  border: none;
-  border-radius: 4px;
-  color: white;
-  background-color: #699CFF;
-  cursor: pointer;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #477EBF;
-  }
-`;
 
 export default JoinEmployeePage;
